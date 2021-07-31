b2e9cf136e064d0ed5746462b96edd162ab4a050
Debora <dkx1003@ubu.alu.es>
2021-07-31 11:03:47 -0300

    Modified CustomBeanPostProcessorTest to verify if it is instance of LifeCycleDemoBeanTest instead of LifeCycleDemoBean

--- src/main/java/guru/springframework/sfgdi/services/CustomBeanPostProcessorTest.java
+++ src/main/java/guru/springframework/sfgdi/services/CustomBeanPostProcessorTest.java
@@ -10,28 +10,28 @@ public class CustomBeanPostProcessorTest implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
 
-        if(bean instanceof LifeCycleDemoBean){
-            ((LifeCycleDemoBean) bean).beforeInit();
-        }
+//        if(bean instanceof LifeCycleDemoBean){
+//            ((LifeCycleDemoBean) bean).beforeInit();
+//        }
 
 //Error creating bean with name 'lifeCycleDemoBeanTest' defined in file sfg-di\target\classes\guru\springframework\sfgdi\services\LifeCycleDemoBeanTest.class]: Initialization of bean failed; nested exception is java.lang.ClassCastException: class guru.springframework.sfgdi.services.LifeCycleDemoBeanTest cannot be cast to class guru.springframework.sfgdi.services.LifeCycleDemoBean (guru.springframework.sfgdi.services.LifeCycleDemoBeanTest and guru.springframework.sfgdi.services.LifeCycleDemoBean are in unnamed module of loader 'app')
 //        at org.springframework.beans.factory.support.AbstractAutowireCapableBeanFactory.d
-//        if(bean instanceof LifeCycleDemoBeanTest){
-//            ((LifeCycleDemoBeanTest) bean).beforeInit();
-//        }
+        if(bean instanceof LifeCycleDemoBeanTest){
+            ((LifeCycleDemoBeanTest) bean).beforeInit();
+        }
 
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        if(bean instanceof LifeCycleDemoBean){
-            ((LifeCycleDemoBean) bean).afterInit();
-        }
-
-//        if(bean instanceof LifeCycleDemoBeanTest){
+//        if(bean instanceof LifeCycleDemoBean){
 //            ((LifeCycleDemoBean) bean).afterInit();
 //        }
+
+        if(bean instanceof LifeCycleDemoBeanTest){
+            ((LifeCycleDemoBean) bean).afterInit();
+        }
 
         return bean;
     }
