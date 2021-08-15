cb3cd6401ee2cb178237ed4c15e06e49827430d0
Debora <dkx1003@ubu.alu.es>
2021-08-15 16:44:58 -0300

    Added different packages to spring context

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -4,10 +4,12 @@ import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages={"guru.springframework.sfgdi","com.springframework.pets"})//Look for
+// specific packages in the spring context
 @SpringBootApplication
 public class SfgDiApplication {
-
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
